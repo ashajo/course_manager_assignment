920d4a04faeb90363389ecd448354bb6ac64f7d5
asha joseph <ashajo183@gmail.com>
2021-01-20 15:16:24 +0100

    StudentDao

---
+++ src/main/java/se/lexicon/course_manager_assignment/model/StudentDao.java
@@ -0,0 +1,16 @@
+package se.lexicon.course_manager_assignment.model;
+
+
+import java.util.List;
+
+
+
+public interface StudentDao {
+    Student saveStudent (Student student);
+    Student findByEmail (String email);
+    List<Student> findByName (String name);
+    Student findById (int id);
+    List<Student>findAll();
+    boolean deleteStudent (Student student);
+
+}
---
+++ src/test/java/se/lexicon/course_manager_assignment/model/StudentDaoTest.java
@@ -0,0 +1,32 @@
+package se.lexicon.course_manager_assignment.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentDaoTest {
+
+    @Test
+    void saveStudent() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
