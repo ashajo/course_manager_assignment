bd786be026d97a0a9ffb9c551c20089198a88c43
asha joseph <ashajo183@gmail.com>
2021-01-20 14:57:43 +0100

    Student

--- src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -1,4 +1,41 @@
 package se.lexicon.course_manager_assignment.model;
 
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
