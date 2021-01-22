1103a932c39be42f0cfeef2bfe410c6b668b4391
asha joseph <ashajo183@gmail.com>
2021-01-22 11:29:51 +0100

    Course

--- src/main/java/se/lexicon/course_manager_assignment/model/Course.java
+++ src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -1,5 +1,81 @@
 package se.lexicon.course_manager_assignment.model;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private List<Student> students;
 
+
+    public Course(int id, String courseName, LocalDate startDate, int weekDuration) {
+        this.id = id;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = new ArrayList<>();
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeakDuration() {
+        return weekDuration;
+    }
+
+    public void setWeakDuration(int weakDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
+        public void register(Student student) {
+            this.students.add(student);
+        }
+        public void unregistered (Student student) {
+        this.students.remove(student);
+
+    }
 }
--- src/test/java/se/lexicon/course_manager_assignment/model/CourseTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/model/CourseTest.java
@@ -1,4 +1,57 @@
 package se.lexicon.course_manager_assignment.model;
 
+import org.junit.jupiter.api.Test;
+
 public class CourseTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getCourseName() {
+    }
+
+    @Test
+    void setCourseName() {
+    }
+
+    @Test
+    void getStartDate() {
+    }
+
+    @Test
+    void setStartDate() {
+    }
+
+    @Test
+    void getWeakDuration() {
+    }
+
+    @Test
+    void setWeakDuration() {
+    }
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void setStudents() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void unregistered() {
+    }
 }
